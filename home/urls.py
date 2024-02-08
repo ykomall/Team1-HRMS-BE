5e3ec36e00f1d70c1d2e813ab84b8b83a2404a48
Dhruv Bhargav <dhruvbhargav@Dhruvs-MacBook-Pro.local>
2024-02-09 00:59:55 +0530

    admin integrated

--- home/urls.py
+++ home/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from home.views import Leave, NewUser, UserLoginView, UserSignupView, ManagerGet, GrantLeave,DeclineUser,LeaveBalance
+from home.views import Leave, NewUser, UserLoginView, UserSignupView, ManagerGet, GrantLeave,DeclineUser,LeaveBalance,ProfileCard,GetManager
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -12,10 +12,12 @@ urlpatterns = [
     path("grantleave",GrantLeave.as_view()),
     path("manager",ManagerGet.as_view()),
     path("addEmployee",NewUser.as_view()),
-    path("getnewEmployee",NewUser.as_view),
+    path("getnewEmployee",NewUser.as_view()),
     path("getleaveapplication",GrantLeave.as_view()),
     path("declineEmployee",DeclineUser.as_view()),
     path("leave_balance",LeaveBalance.as_view()),
+    path("profilecard",ProfileCard.as_view()),
+    path("getManager",GetManager.as_view()),
     path('api/token', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
 ]
